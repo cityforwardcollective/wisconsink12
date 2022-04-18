a615af4d16de16fe6d37a8042c201328211d068d
Spencer Schien <spencer.schien@cityforwardcollective.org>
2022-04-18 12:08:26 -0500

    updated package description

---
+++ R/wisconsink12.R
@@ -0,0 +1,10 @@
+#' wisconsink12: A package for easy access to Wisconsin K12 school data
+#'
+#' The wisconsink12 package provides publicly-available data on public and private
+#' Wisconsin K12 schools in a coherent relational database structure.
+#'
+#' @useDynLib wisconsink12, .registration = TRUE
+#' @docType package
+#' @name wisconsink12
+NULL
+#> NULL
