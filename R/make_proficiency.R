32af0072e5608853fcb093cd6ae515e833fd7546
Spencer Schien <spencer.schien@gmail.com>
2023-01-11 14:27:50 -0600

    added make_proficiency

---
+++ R/make_proficiency.R
@@ -0,0 +1,99 @@
+#' Make common dataframes
+#'
+#' Creates commonly-needed dataframes of \code{wisconsink12} data,
+#' such as city of Milwaukee schools and their report cards.
+#'
+#' @param private_type Select which type of Report Card to be included for choice schools.
+#' Options are 'choice' for the 'Private - Choice Students' and 'all' for 'Private - All Students'
+#' Report Card types.
+#' @param exclude_milwaukee Logical. If TRUE (default value), Milwaukee schools will be
+#' excluded from \code{wi_rc}.
+#'
+#' @import dplyr
+#' @import tidyr
+#'
+#' @export make_proficiency
+
+# Make Proficiency =================================================================
+
+make_proficiency <- function() {
+
+  # public act
+
+  act_test_results <- act |>
+    right_join(schools, by = c("school_year", "dpi_true_id")) |>
+    filter(group_by_value == "All Students" &
+             test_subject %in% c("ELA", "Mathematics") &
+             accurate_agency_type != "Private") |>
+    mutate(pa = case_when(test_result %in% c("Below Basic", "Basic") ~ "bb",
+                          test_result %in% c("Proficient", "Advanced") ~ "pa",
+                          test_result == "No Test" ~ "no_test",
+                          TRUE ~ "REDACTED")) |>
+    filter(pa != "REDACTED")
+
+  # private act
+  private  <- schools |>
+    filter(accurate_agency_type == "Private") |>
+    select(dpi_true_id, school_year, accurate_agency_type) |>
+    left_join(forward_exam, by = c("school_year", "dpi_true_id")) |>
+    filter(test_group == "ACT") |>
+    mutate(pa = case_when(test_result %in% c("Below Basic", "Basic") ~ "bb",
+                          test_result %in% c("Proficient", "Advanced") ~ "pa",
+                          test_result == "No Test" ~ "no_test",
+                          TRUE ~ "REDACTED")) |>
+    filter(pa != "REDACTED" & test_subject %in% c("ELA", "Mathematics"))
+
+  # aspire
+
+  aspire <- forward_exam |>
+    filter(group_by_value == "All Students" &
+             test_group == "Aspire" &
+             test_subject %in% c("ELA", "Mathematics")) |>
+    right_join(schools, by = c("school_year", "dpi_true_id")) |>
+    mutate(pa = case_when(test_result %in% c("Below Basic", "Basic") ~ "bb",
+                          test_result %in% c("Proficient", "Advanced") ~ "pa",
+                          test_result == "No Test" ~ "no_test",
+                          TRUE ~ "REDACTED")) |>
+    filter(pa != "REDACTED")
+
+  all <- bind_rows(aspire, private, act_test_results)
+
+  # summarise at school level for hs grades
+
+  hs <- all |>
+    group_by(school_year, dpi_true_id, test_subject, pa) |>
+    summarise(total_count = sum(student_count, na.rm = TRUE))
+
+  # see main.R for source of `f`
+  # contains Forward
+
+  elem <- forward_exam |>
+    filter(group_by_value == "All Students" &
+             test_group == "Forward" &
+             test_subject %in% c("ELA", "Mathematics")) |>
+    right_join(schools, by = c("school_year", "dpi_true_id")) |>
+    mutate(pa = case_when(test_result %in% c("Below Basic", "Basic") ~ "bb",
+                          test_result %in% c("Proficient", "Advanced") ~ "pa",
+                          test_result == "No Test" ~ "no_test",
+                          TRUE ~ "REDACTED")) |>
+    group_by(school_year, dpi_true_id, test_subject, pa) |>
+    summarise(total_count = sum(student_count, na.rm = TRUE))
+
+
+  all_prof <- bind_rows(hs, elem) |>
+    group_by(school_year,
+             dpi_true_id,
+             test_subject,
+             pa) |>
+    summarise(total_count = sum(total_count)) |>
+    ungroup() |>
+    group_by(school_year, dpi_true_id, test_subject) |>
+    mutate(perc = (total_count / sum(total_count))) |>
+    filter(pa == "pa") |>
+    # select(-total_count) |>
+    ungroup()
+
+  return(all_prof)
+}
+
+# prof <- make_proficiency()
